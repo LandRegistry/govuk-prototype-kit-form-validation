a0f48ee0b65a54424dedb775225213c94953a88a
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2019-10-24 12:00:25 +0100

    validation code

---
+++ app/assets/javascripts/custom-validator.js
@@ -0,0 +1,8 @@
+
+validate.validators.postcode = function(value, options, key, attributes) {
+ console.log(value);
+ console.log(options);
+ console.log(key);
+ console.log(attributes);
+ return "is totally wrong";
+};
--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.use('/node_modules', express.static('node_modules'))
 
 
 // /////////////////////////
